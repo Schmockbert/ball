7f11b54431dc239a0c5ad02d3f9c7a8a127bbe3d
Schmockbert <schlomo.schmock@gmail.com>
2013-12-27 20:59:28 +0100

    v0.2

--- gen/fhfl/mogensen/ball/R.java
+++ gen/fhfl/mogensen/ball/R.java
@@ -10,6 +10,8 @@ package fhfl.mogensen.ball;
 public final class R {
     public static final class array {
         public static final int colours_array=0x7f060000;
+        public static final int lives_array=0x7f060002;
+        public static final int speed_array=0x7f060001;
     }
     public static final class attr {
     }
@@ -27,14 +29,20 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+        public static final int backgroundColour=0x7f090007;
+        public static final int backgroundColourPicker=0x7f090006;
         public static final int circleColour=0x7f090003;
         public static final int circleColourPicker=0x7f090002;
         public static final int finishColour=0x7f090005;
         public static final int finishColourPicker=0x7f090004;
-        public static final int help=0x7f090008;
-        public static final int mainmenu=0x7f090007;
-        public static final int reset=0x7f090006;
+        public static final int help=0x7f09000e;
+        public static final int livesPick=0x7f09000b;
+        public static final int livesPicker=0x7f09000a;
+        public static final int mainmenu=0x7f09000d;
+        public static final int reset=0x7f09000c;
         public static final int settings=0x7f090000;
+        public static final int speedPick=0x7f090009;
+        public static final int speedPicker=0x7f090008;
         public static final int start=0x7f090001;
     }
     public static final class layout {
--- src/fhfl/mogensen/ball/Customize.java
+++ src/fhfl/mogensen/ball/Customize.java
@@ -7,14 +7,22 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class Customize extends Activity implements OnItemSelectedListener
 {
-public static int[] colours;
-static Spinner circleColourPicker;
-static Spinner finishColourPicker;
+int[] colours;
+int[] speed;
+Spinner circleColourPicker;
+Spinner finishColourPicker;
+Spinner backgroundColourPicker;
+Spinner speedPicker;
+Spinner livesPicker;
 static int circlePickedColour;
 static int finishPickedColour;
+static int backgroundPickedColour;
+static int pickedSpeed;
+static int pickedLives;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState)
@@ -27,18 +35,37 @@ static int finishPickedColour;
 	    //Reference UI-elements
 	    circleColourPicker = (Spinner) findViewById(R.id.circleColourPicker);
 	    finishColourPicker = (Spinner) findViewById(R.id.finishColourPicker);
+	    backgroundColourPicker = (Spinner) findViewById(R.id.backgroundColourPicker);
+	    speedPicker = (Spinner) findViewById(R.id.speedPicker);
+	    livesPicker = (Spinner) findViewById(R.id.livesPicker);
 	    circleColourPicker.setOnItemSelectedListener(this);
 	    finishColourPicker.setOnItemSelectedListener(this);
+	    backgroundColourPicker.setOnItemSelectedListener(this);
+	    speedPicker.setOnItemSelectedListener(this);
+	    livesPicker.setOnItemSelectedListener(this);
 	        
 	    //Create an ArrayAdapter using the string array and a default spinner layout
-	    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.colours_array, android.R.layout.simple_spinner_item);
+	    ArrayAdapter<CharSequence> coloursAdapter = ArrayAdapter.createFromResource(this, R.array.colours_array, android.R.layout.simple_spinner_item);
+	    ArrayAdapter<CharSequence> difficultyAdapter = ArrayAdapter.createFromResource(this, R.array.speed_array, android.R.layout.simple_spinner_item);
+	    ArrayAdapter<CharSequence> livesAdapter = ArrayAdapter.createFromResource(this, R.array.lives_array, android.R.layout.simple_spinner_item);
 	    //Specify the layout to use when the list of choices appears
-	    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+	    coloursAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+	    difficultyAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+	    livesAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 	    // Apply the adapter to the spinner
-	    circleColourPicker.setAdapter(adapter);
-	    finishColourPicker.setAdapter(adapter);
+	    circleColourPicker.setAdapter(coloursAdapter);
+	    finishColourPicker.setAdapter(coloursAdapter);
+	    backgroundColourPicker.setAdapter(coloursAdapter);
+	    speedPicker.setAdapter(difficultyAdapter);
+	    livesPicker.setAdapter(livesAdapter);
+	    livesPicker.setSelection(2);
+	    speedPicker.setSelection(0);
+	    circleColourPicker.setSelection(5);
+	    finishColourPicker.setSelection(3);
+	    backgroundColourPicker.setSelection(7);
         colours = new int[] {0xff000000, 0xff0000ff, 0xff00ffff, 0xff00ff00, 0xffff00ff,
-        		0xffff0000, 0xffffff00};	    
+        		0xffff0000, 0xffffff00, 0x00000000};
+        speed = new int[] {1, 2, 3, 4, 5};
 	}
 
     @Override
@@ -50,9 +77,26 @@ static int finishPickedColour;
         {
         	circlePickedColour = colours[pos];                
         }
-        else if(spinner.getId() == R.id.finishColourPicker)
+        if(spinner.getId() == R.id.finishColourPicker)
         {
           	finishPickedColour = colours[pos];
+        }
+        if(spinner.getId() == R.id.backgroundColourPicker)
+        {
+        	backgroundPickedColour = colours[pos];
+        	if (backgroundPickedColour == circlePickedColour || backgroundPickedColour == finishPickedColour)
+        	{
+        		backgroundColourPicker.setSelection(7);
+        		Toast.makeText(getApplicationContext(), "Background Colour can not be the same as circle or finish colour. Defaulted.", Toast.LENGTH_LONG).show();
+        	}
+        }
+        if(spinner.getId() == R.id.speedPicker)
+        {
+        	pickedSpeed = speed[pos];
+        }
+        if(spinner.getId() == R.id.livesPicker)
+        {
+        	pickedLives = speed[pos];
         }
         }
 
